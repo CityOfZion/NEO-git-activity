10f1933bd794449ef66bd8a54d795259cc35b0b0
fabwa <fabian.wahle@gmail.com>
2019-08-31 11:53:06 +0200

    Update constants.js

--- src/helper/constants.js
+++ src/helper/constants.js
@@ -1,4 +1,4 @@
-export var users = ["neo-project","neo-ngd","CityOfZion","NewEconoLab","NeoResearch"]
+export var users = ["neo-project","neo-ngd","CityOfZion","NewEconoLab","NeoResearch","nspcc-dev"]
 
 export var TIME_INTERVAL = 600000//
 export var MAX_PROJECTS_COUNT = 20
